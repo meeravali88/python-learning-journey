fe69afafc2dc9ca9f94e0b897425ce6c0b8c40ec
meeravali88 <mdhachepalli@gmail.com>
2026-01-28 21:46:08 +0530

    Add comments

---
+++ comments.py
@@ -0,0 +1,13 @@
+# Read three integers from input and print them each on a new line
+a = int(input())
+b = int(input())
+c = int(input())
+# aash for single line comment 
+# if single line comment is needed we can use the above syntax
+print(a)
+print(b)
+print(c)
+# if a multi line comment is needed we can use the below syntax
+'''
+This is a multi-line comment
+'''
