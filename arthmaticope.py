8ed7a137f5a3848120045f8673009c9047525220
meeravali88 <mdhachepalli@gmail.com>
2026-01-30 01:03:14 +0530

    simple arthametic operations

---
+++ arthmaticope.py
@@ -0,0 +1,21 @@
+#simple arthamatic operators
+a = 10
+b = 5
+#adding two numbers
+addition = a + b
+#subtracting two numbers
+subtraction = a - b
+#multiplying two numbers
+multiplication = a * b
+#dividing two numbers
+division = a / b
+#modulus of two numbers
+modulus = a % b
+#displaying the results 
+print("Addition:", addition)
+print("Subtraction:", subtraction)  
+print("Multiplication:", multiplication)
+print("Division:", division)
+print("Modulus:", modulus)
+
+#end of the code
