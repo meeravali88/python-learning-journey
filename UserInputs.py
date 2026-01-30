35c07866f2861bd947e37725905e99d1ddcbae33
meeravali88 <mdhachepalli@gmail.com>
2026-01-30 22:46:41 +0530

    Taking User Inputs and printing the output

---
+++ UserInputs.py
@@ -0,0 +1,13 @@
+
+# Take string input and print the string
+s = input()
+print(s)
+
+# Take integer input and add 10 to the integer input and print
+n = int(input())
+print(n+10)
+
+# Take floating-point input and multiply the float input by 10 and print
+f = float(input())
+print(f*10)
+
