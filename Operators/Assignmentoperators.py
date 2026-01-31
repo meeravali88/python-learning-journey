aeb185ccb7d97c3cca605a647a5fceb4b357bf31
meeravali88 <mdhachepalli@gmail.com>
2026-01-31 22:48:54 +0530

    Assignment operators

---
+++ Operators/Assignmentoperators.py
@@ -0,0 +1,15 @@
+#Assignment Operators in Python
+a = 10
+b = 5
+# Using += operator
+a += b  # Equivalent to a = a + b
+print(a)
+# Using -= operator
+a -= b  # Equivalent to a = a - b
+print(a)
+# Using *= operator
+a *= b  # Equivalent to a = a * b
+print(a)
+# Using /= operator
+a /= b  # Equivalent to a = a / b
+print(a)
