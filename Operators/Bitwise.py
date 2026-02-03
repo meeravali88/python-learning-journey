b1326e5fa67b864ae18c355cee40e7da12bc2994
meeravali88 <mdhachepalli@gmail.com>
2026-02-03 17:07:14 +0530

    Updated bitwise operators example

--- Operators/Bitwise.py
+++ Operators/Bitwise.py
@@ -1,2 +1,17 @@
-s = input(bin(5))
-print(s)
+# bitwise operators in Python
+a = 5 
+b = 3 
+# Bitwise AND operator
+bitwise_and = a & b  
+# Bitwise OR operator
+bitwise_or = a | b  
+# Bitwise XOR operator
+bitwise_xor = a ^ b  
+# Bitwise NOT operator
+bitwise_not_a = ~a 
+bitwise_not_b = ~b  
+
+print(bitwise_and)
+print(bitwise_or)
+print(bitwise_xor)
+print(bitwise_not_a)
