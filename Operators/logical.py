f064a721a43842525716fa88cefe120bcd10ceab
meeravali88 <mdhachepalli@gmail.com>
2026-02-05 22:28:38 +0530

    Logical operators in python

---
+++ Operators/logical.py
@@ -0,0 +1,14 @@
+# logical operators in Python
+x = True
+y = False
+# Logical AND operator
+logical_and = x and y
+# Logical OR operator
+logical_or = x or y
+# Logical NOT operator
+logical_not_x = not x
+
+print(logical_and)
+print(logical_or)
+print(logical_not_x)
+
