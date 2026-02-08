63c4985cb087f08bfd32a603c6bb9f14d8d7359c
meeravali88 <mdhachepalli@gmail.com>
2026-02-08 16:53:54 +0530

    Elif used to check multiple conditions

---
+++ Conditional Statments/Elif.py
@@ -0,0 +1,12 @@
+# Elif statements in python
+a = 10
+b = 20
+c = 30
+
+if a > b:
+    print("a is greater than b")
+# Using elif to check another condition
+elif a > c:
+    print("a is greater than c")
+else:
+    print("a is not greater than b or c")
