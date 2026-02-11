94940ebac9b3e3a258a25657fbf19962e53f383a
meeravali88 <mdhachepalli@gmail.com>
2026-02-11 11:38:31 +0530

    new conditional statment in python

---
+++ Conditional Statments/Ternary .py
@@ -0,0 +1,6 @@
+# Ternary conditional statement in Python
+a = 10
+b = 20
+# using ternary condition we can write the if-else statement in a single line
+result = "a is greater than b" if a > b else "a is less than or equal to b"
+print(result)
