95b492e130bfa8f551e06c9679cddfb7a2c79f5b
meeravali88 <mdhachepalli@gmail.com>
2026-02-12 08:04:50 +0530

    Functions python

---
+++ Fonctions/calling a function.py
@@ -0,0 +1,9 @@
+# we can define a function using the def keyword, followed by the name of the function and parentheses
+
+def greet():
+    
+    print("This is my first function in Python!")
+    
+# to print the msg we need to call the function using its name followed by parentheses
+
+greet()  # Output: This is my first function in Python!
