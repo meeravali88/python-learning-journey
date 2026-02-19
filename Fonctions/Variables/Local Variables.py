76a663aae802c353b34408b1d3be5897d680cbac
meeravali88 <mdhachepalli@gmail.com>
2026-02-19 22:55:54 +0530

    Local var

---
+++ Fonctions/Variables/Local Variables.py
@@ -0,0 +1,10 @@
+# Local Variables in Functions
+
+def greet():
+    # Local variable defined inside the function
+    msg = "Hello from inside the function!"
+    print(msg)
+# Calling the function to see the local variable in action
+greet()
+
+# Trying to access the local variable outside the function will result in an error
